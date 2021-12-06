0e16f9231492aec62b44e03df3eb86d611f8dc92
Angelika M <10550@student.puzim.edu.pl>
2021-12-06 12:48:53 +0100

    Dodanie obslugi wyjatkow

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace mpro_lab_01
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            string Imie = "Angelika";
+        {   StreamReader sr = null;
+            try {
+            
+            sr = new StreamReader("/home/linuxmint/Desktop/doc");
+            Console.WriteLine(sr.ReadToEnd());
+        
+            }
+            
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"File not found {ex.FileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                
+                sr?.Close();
+            }
         }
     }
 }
